356e91b0bde58ea48a9ed33ced2d58ca3ee2af55
Alexander-Goryaynov <gor.alex595@gmail.com>
2021-03-01 21:30:32 +0400

    Init

---
+++ OpenGlApp/FormGL.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace OpenGlApp
+{
+    partial class FormGL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl = new SharpGL.OpenGLControl();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.Location = new System.Drawing.Point(12, 12);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(776, 426);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            this.openGLControl.Load += new System.EventHandler(this.openGLControl_Load);
+            // 
+            // FormGL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 521);
+            this.Controls.Add(this.openGLControl);
+            this.Name = "FormGL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormGL";
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl;
+    }
+}
---
+++ OpenGlApp/FormGL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+
+namespace OpenGlApp
+{
+    public partial class FormGL : Form
+    {
+        OpenGL gl;
+        public FormGL()
+        {
+            InitializeComponent();
+            gl = openGLControl.OpenGL;
+        }
+
+        private void openGLControl_Load(object sender, EventArgs e)
+        {
+            openGLControl.Width = Width - 30;
+            openGLControl.Height = Height - 100;
+            // задаём проекционную матрицу
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // загружаем матрицу в стек
+            gl.LoadIdentity();
+            // матрица проекции (угол обзора, соотношение сторон экрана,
+            // расстояние до ближней плоскости, расстояние до дальней плоскости)
+            gl.Perspective(80, 4 / 3, .1, 200);
+            // матрица вида (положение камеры x0, y0, z0; в какую точку x1, y1, z1 она смотрит;
+            // величины поворота "головы" камеры x2, y2, z2)
+            gl.LookAt(10, 5, 10, 0, 1, 0, 0, 1, 0);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.LoadIdentity();
+            // цвет фона
+            gl.ClearColor(.1f, .1f, .3f, 1);
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+        }
+
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            gl.Begin(OpenGL.GL_POLYGON);
+            gl.Color(255, 0, 0);
+            gl.Vertex(0, 0, 2.5);
+            gl.Vertex(5, 0, 2.5);
+            gl.Vertex(5, 2.5, 2.5);
+            gl.Vertex(0, 2.5, 2.5);
+            gl.End();
+        }
+    }
+}
