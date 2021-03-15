18579f8e0ede466190ed6d92cbd55c66611fecb2
Alexander-Goryaynov <gor.alex595@gmail.com>
2021-03-16 00:55:18 +0400

    Add 3d meshes, lighting, textures

--- OpenGlApp/FormGlExtended.cs
+++ OpenGlApp/FormGlExtended.cs
@@ -8,11 +8,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharpGL.SceneGraph;
+using System.Drawing.Imaging;
 
 namespace OpenGlApp
 {
     public partial class FormGlExtended : Form
     {
+        private struct TextureStruct
+        {
+            public uint TextureValue;
+            public double[,] X_YText;
+            public double[,] CoordX_YText;
+        }
+
         private const int gridLinesCount = 14;
         // длина ребра "1" - "2"
         private const float a = 4.0f;
@@ -39,6 +48,9 @@ namespace OpenGlApp
         private const double objStep = 1.0;
         // шаг радиуса камеры при зуме
         private const double zoomStep = 1.0;
+        // включить/выключить освещение
+        private bool isLightingEnabled;
+
         OpenGL gl;
         public FormGlExtended()
         {
@@ -52,6 +64,7 @@ namespace OpenGlApp
             camTimer.Tick += new EventHandler(rotateCamera);
             camTimer.Interval = camTimerInterval;
             isCamTimerActive = false;
+            isLightingEnabled = false;
             x = 0;
         }
 
@@ -74,12 +87,20 @@ namespace OpenGlApp
             // цвет фона
             gl.ClearColor(.1f, .1f, .3f, 1);
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            if (isLightingEnabled)
+            {
+                EnableLighting();
+            }
         }
 
         private void openGLControl_OpenGLDraw(object sender, RenderEventArgs args)
         {
             DrawGrid();
-            DrawObject();
+            //DrawObject();
+            //DrawSpheres();
+
+            //DrawCylinder();
+            displayTextureFromFile(@"C:\Users\Александр\Desktop\unknown.bmp");
         }
 
         private void DrawGrid()
@@ -119,6 +140,95 @@ namespace OpenGlApp
             gl.Vertex(0.0f, 0.0f, 0.0f);
             gl.Vertex(0.0f, 0.0f, 10.0f);
             gl.End();
+        }
+
+        private void DrawSpheres()
+        {
+            // Зелёная сфера
+            var i = gl.NewQuadric();
+            gl.Color(Color.Green);
+            gl.QuadricDrawStyle(i, OpenGL.GLU_LINE);
+            gl.Sphere(i, 2, 25, 25);
+            // Красная сфера
+            i = gl.NewQuadric();
+            gl.PushMatrix();
+            gl.Translate(-5, 5, 5);
+            gl.Rotate(90, 0, 0);
+            gl.Color(Color.Red);
+            gl.QuadricDrawStyle(i, OpenGL.GLU_FILL);
+            gl.Sphere(i, 2, 25, 25);
+            gl.PopMatrix();
+            // Синяя сфера
+            i = gl.NewQuadric();
+            gl.PushMatrix();
+            gl.Translate(5, 5, 5);
+            gl.Rotate(90, 0, 0);
+            gl.Color(Color.Blue);
+            gl.QuadricDrawStyle(i, OpenGL.GLU_FILL);
+            gl.Sphere(i, 2, 25, 25);
+            gl.PopMatrix();
+        }
+
+        private void DrawCylinder()
+        {
+            var i = gl.NewQuadric();
+            gl.PushMatrix();
+            gl.Translate(1, -10, 1);
+            gl.Rotate(50, 60, 0);
+            gl.QuadricDrawStyle(i, OpenGL.GLU_FILL);
+            gl.Cylinder(i, 5, 2, 10, 25, 25);
+            gl.PopMatrix();
+        }
+
+        private void EnableLighting()
+        {
+            // цвет освещения по RGB
+            float[] light0_diffuse3 = { 1f, 1f, 1f };
+            float[] light0_direction3 = { 4.0f, 1f, 3.0f, 1.0f };
+            gl.Enable(OpenGL.GL_LIGHTING);
+            gl.Enable(OpenGL.GL_LIGHT3);
+            gl.Light(OpenGL.GL_LIGHT3, OpenGL.GL_DIFFUSE, light0_diffuse3);
+            gl.Light(OpenGL.GL_LIGHT3, OpenGL.GL_POSITION, light0_direction3);
+        }
+
+        private void DisableLighting()
+        {
+            gl.Disable(OpenGL.GL_LIGHTING);
+            gl.Disable(OpenGL.GL_LIGHT3);
+        }
+
+        private void displayTextureFromFile(string path)
+        {
+            TextureStruct Textures = new TextureStruct();
+            Textures.CoordX_YText = new double[4, 3] { { 8, 0, 8 }, { 0, 0, 8 }, { 0, 0, 0 }, { 8, 0, 0 } };
+            Textures.X_YText = new double[4, 2] { { 0, 0 }, { 1, 0 }, { 1, 1 }, { 0, 1 } };
+            Bitmap BmpImage = new Bitmap(new Bitmap(path), 512, 512);
+            Rectangle rect = new Rectangle(0, 0, BmpImage.Width, BmpImage.Height);
+            BitmapData BmpData = BmpImage.LockBits(rect, ImageLockMode.ReadOnly, BmpImage.PixelFormat);
+            gl.TexImage2D(
+                OpenGL.GL_TEXTURE_2D,
+                0,
+                (int)OpenGL.GL_RGBA,
+                BmpImage.Width,
+                BmpImage.Height,
+                0,
+                OpenGL.GL_BGRA,
+                OpenGL.GL_UNSIGNED_BYTE,
+                BmpData.Scan0
+            );
+            BmpImage.UnlockBits(BmpData);
+            gl.BindTexture(OpenGL.GL_TEXTURE_2D, Textures.TextureValue);
+            gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MIN_FILTER, OpenGL.GL_LINEAR);
+            gl.TexParameter(OpenGL.GL_TEXTURE_2D, OpenGL.GL_TEXTURE_MAG_FILTER, OpenGL.GL_LINEAR);
+            gl.Enable(OpenGL.GL_TEXTURE_2D);
+            gl.Begin(OpenGL.GL_QUADS);
+            for (int i = 0; i < 4; i++)
+            {
+                gl.TexCoord(Textures.X_YText[i, 0], Textures.X_YText[i, 1]);
+                gl.Vertex(Textures.CoordX_YText[i, 0], Textures.CoordX_YText[i, 1], Textures.CoordX_YText[i, 2]);
+            }
+            gl.End();
+            gl.Disable(OpenGL.GL_TEXTURE_2D);
         }
 
         private void DrawObject()
@@ -212,6 +322,20 @@ namespace OpenGlApp
 
         private void openGLControl_Click(object sender, EventArgs e)
         {
+            // Process right button click
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (me.Button == MouseButtons.Right)
+            {
+                if (isLightingEnabled)
+                {
+                    DisableLighting();
+                }
+                else
+                {
+                    EnableLighting();
+                }
+                return;
+            }
             if (isCamTimerActive)
             {
                 camTimer.Stop();
